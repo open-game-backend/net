a7d43c37eb94e749e7506c45f546053658aec077
Nick Prühs <dev@npruehs.de>
2020-07-22 17:48:50 +0200

    Add convenience constructor for ServerSendHeartbeatRequest.

--- src/main/java/de/opengamebackend/matchmaking/model/requests/ServerSendHeartbeatRequest.java
+++ src/main/java/de/opengamebackend/matchmaking/model/requests/ServerSendHeartbeatRequest.java
@@ -3,6 +3,11 @@ package de.opengamebackend.matchmaking.model.requests;
 public class ServerSendHeartbeatRequest {
     private String id;
 
+    public ServerSendHeartbeatRequest() { }
+    public ServerSendHeartbeatRequest(String id) {
+        this.id = id;
+    }
+
     public String getId() {
         return id;
     }
