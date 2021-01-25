058d65278a8fdcf18bce3a24101320fb1f2aa2f5
Nick Prühs <dev@npruehs.de>
2021-01-25 21:01:23 +0100

    Add paging for getting players.

--- src/main/java/de/opengamebackend/auth/model/responses/GetPlayersResponse.java
+++ src/main/java/de/opengamebackend/auth/model/responses/GetPlayersResponse.java
@@ -5,13 +5,17 @@ import java.util.List;
 
 public class GetPlayersResponse {
     private List<GetPlayersResponsePlayer> players;
+    private int totalPlayers;
+    private int totalPages;
 
     public GetPlayersResponse() {
         this.players = new ArrayList<>();
     }
 
-    public GetPlayersResponse(List<GetPlayersResponsePlayer> players) {
+    public GetPlayersResponse(List<GetPlayersResponsePlayer> players, int totalPlayers, int totalPages) {
         this.players = players;
+        this.totalPlayers = totalPlayers;
+        this.totalPages = totalPages;
     }
 
     public List<GetPlayersResponsePlayer> getPlayers() {
@@ -20,5 +24,21 @@ public class GetPlayersResponse {
 
     public void setPlayers(List<GetPlayersResponsePlayer> players) {
         this.players = players;
+    }
+
+    public int getTotalPlayers() {
+        return totalPlayers;
+    }
+
+    public void setTotalPlayers(int totalPlayers) {
+        this.totalPlayers = totalPlayers;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
     }
 }
