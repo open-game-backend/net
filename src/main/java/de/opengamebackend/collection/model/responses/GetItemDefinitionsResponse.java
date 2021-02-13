31d58c970bd8aeb6c61f5411636ef0dc5cdd6879
Nick Prühs <dev@npruehs.de>
2021-02-13 23:51:03 +0100

    Remove item tags from GetitemDefinitionsResponse.

--- src/main/java/de/opengamebackend/collection/model/responses/GetItemDefinitionsResponse.java
+++ src/main/java/de/opengamebackend/collection/model/responses/GetItemDefinitionsResponse.java
@@ -3,23 +3,13 @@ package de.opengamebackend.collection.model.responses;
 import java.util.List;
 
 public class GetItemDefinitionsResponse {
-    private List<String> itemTags;
     private List<GetItemDefinitionsResponseItem> itemDefinitions;
 
     public GetItemDefinitionsResponse() {
     }
 
-    public GetItemDefinitionsResponse(List<String> itemTags, List<GetItemDefinitionsResponseItem> itemDefinitions) {
-        this.itemTags = itemTags;
+    public GetItemDefinitionsResponse(List<GetItemDefinitionsResponseItem> itemDefinitions) {
         this.itemDefinitions = itemDefinitions;
-    }
-
-    public List<String> getItemTags() {
-        return itemTags;
-    }
-
-    public void setItemTags(List<String> itemTags) {
-        this.itemTags = itemTags;
     }
 
     public List<GetItemDefinitionsResponseItem> getItemDefinitions() {
