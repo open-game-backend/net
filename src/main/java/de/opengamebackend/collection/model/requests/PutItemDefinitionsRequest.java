91d9dfb44da1797d202cc6cb88e95a0aaf1ee311
Nick Prühs <dev@npruehs.de>
2021-02-13 23:50:40 +0100

    Remove item tags from PutItemDefinitionsRequest.

--- src/main/java/de/opengamebackend/collection/model/requests/PutItemDefinitionsRequest.java
+++ src/main/java/de/opengamebackend/collection/model/requests/PutItemDefinitionsRequest.java
@@ -4,20 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PutItemDefinitionsRequest {
-    private List<String> itemTags;
     private List<PutItemDefinitionsRequestItem> itemDefinitions;
 
     public PutItemDefinitionsRequest() {
-        itemTags = new ArrayList<>();
         itemDefinitions = new ArrayList<>();
-    }
-
-    public List<String> getItemTags() {
-        return itemTags;
-    }
-
-    public void setItemTags(List<String> itemTags) {
-        this.itemTags = itemTags;
     }
 
     public List<PutItemDefinitionsRequestItem> getItemDefinitions() {
