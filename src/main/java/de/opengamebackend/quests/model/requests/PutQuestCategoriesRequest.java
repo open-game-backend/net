4d167d35d098d97dc8a7b4687859eafa9cb62455
Nick Prühs <dev@npruehs.de>
2021-04-04 22:18:50 +0200

    Add default constructors for quest requests.

--- src/main/java/de/opengamebackend/quests/model/requests/PutQuestCategoriesRequest.java
+++ src/main/java/de/opengamebackend/quests/model/requests/PutQuestCategoriesRequest.java
@@ -1,9 +1,14 @@
 package de.opengamebackend.quests.model.requests;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PutQuestCategoriesRequest {
     private List<PutQuestCategoriesRequestItem> categories;
+
+    public PutQuestCategoriesRequest() {
+        categories = new ArrayList<>();
+    }
 
     public List<PutQuestCategoriesRequestItem> getCategories() {
         return categories;
--- src/main/java/de/opengamebackend/quests/model/requests/PutQuestDefinitionsRequest.java
+++ src/main/java/de/opengamebackend/quests/model/requests/PutQuestDefinitionsRequest.java
@@ -1,9 +1,14 @@
 package de.opengamebackend.quests.model.requests;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PutQuestDefinitionsRequest {
     private List<PutQuestDefinitionsRequestItem> questDefinitions;
+
+    public PutQuestDefinitionsRequest() {
+        questDefinitions = new ArrayList<>();
+    }
 
     public List<PutQuestDefinitionsRequestItem> getQuestDefinitions() {
         return questDefinitions;
